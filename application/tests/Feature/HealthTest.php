d6d9ebd02709e38194621be55b567921fa9c0d2e
Daniil <daniil@gmail.com>
2026-08-12 19:50:22 +0500

    Health checking URL

---
+++ application/tests/Feature/HealthTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HealthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testHealth(): void
+    {
+        $response = $this->get('/api/health');
+
+        $response->assertStatus(200);
+        $response->assertJson(["success" => true]);
+    }
+}
