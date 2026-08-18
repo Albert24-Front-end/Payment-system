fc200dd62c12efbcf3b1ec57e32bff67c53f86c8
Albert24-Front-end <alievalbert2000@mail.ru>
2026-08-18 16:26:57 +0500

    health checking url added and tested

---
+++ application/app/Http/Controllers/HealthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HealthController
+{
+    public function __invoke(): array
+    {
+        return ['success' => true, 'message' => 'OK'];
+    }
+}
